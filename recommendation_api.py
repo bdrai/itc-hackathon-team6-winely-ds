8b7656de16364fdf766eb76e1796cda77a48cb9e
Benjamin Drai <benjamin.drai@gmail.com>
2023-01-25 19:16:22 +0200

    Recommendation API

---
+++ recommendation_api.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, request
+from recommendation_system import RecommendationSystem
+from env import Env
+
+app = Flask(__name__)
+
+
+@app.route('/train_model', methods=['POST'])
+def train_model():
+    inputs = json.loads(request.get_json())
+    env = Env()
+    rs = RecommendationSystem(env=env, **inputs)
+    rs.run()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
